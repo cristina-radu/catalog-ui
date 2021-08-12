2b8ab2428143de8faa7338c80959be493d44f286
cradu <Christinutza88.12345678>
2021-08-12 13:53:06 +0300

    change in style

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -9,6 +9,7 @@ import {MatDialogModule} from "@angular/material/dialog";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatDividerModule} from "@angular/material/divider";
+import {MatPaginatorModule} from "@angular/material/paginator";
 
 @NgModule({
   imports: [
